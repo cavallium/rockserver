02eea5ac661e2749af86491ac0daeab849af2dbe
Andrea Cavalli <andrea@cavallium.it>
2024-10-17 23:10:34 +0200

    Allow keys without array

--- src/main/java/it/cavallium/rockserver/core/common/Keys.java
+++ src/main/java/it/cavallium/rockserver/core/common/Keys.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 import org.jetbrains.annotations.NotNull;
 
-public record Keys(@NotNull MemorySegment @NotNull [] keys) {
+public record Keys(@NotNull MemorySegment @NotNull ... keys) {
 
 	@Override
 	public String toString() {
