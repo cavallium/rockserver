16863923a88cef54425c75f49e7e4fa927c9cb0b
Andrea Cavalli <andrea@cavallium.it>
2025-01-18 00:25:47 +0100

    Do not create new threads, to avoid having RocksDB leaks

--- src/main/java/it/cavallium/rockserver/core/impl/RWScheduler.java
+++ src/main/java/it/cavallium/rockserver/core/impl/RWScheduler.java
@@ -2,8 +2,11 @@ package it.cavallium.rockserver.core.impl;
 
 import static reactor.core.scheduler.Schedulers.DEFAULT_BOUNDED_ELASTIC_QUEUESIZE;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import java.util.concurrent.Executor;
-import reactor.core.publisher.Flux;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
@@ -15,10 +18,23 @@ public record RWScheduler(Scheduler read, Scheduler write, Executor readExecutor
 	}
 
 	public RWScheduler(int readCap, int writeCap, String name) {
-		this(
-				Schedulers.newBoundedElastic(readCap, DEFAULT_BOUNDED_ELASTIC_QUEUESIZE, name + "-read"),
-				Schedulers.newBoundedElastic(writeCap, DEFAULT_BOUNDED_ELASTIC_QUEUESIZE, name + "-write")
+		this(createScheduler(readCap, name + "-read"), createScheduler(writeCap, name + "-write"));
+	}
+
+	private static Scheduler createScheduler(int cap, String name) {
+		var threadFactory = new ThreadFactoryBuilder()
+				.setDaemon(false)
+				.setNameFormat(name + "-%d")
+				.build();
+		var readPool = new ThreadPoolExecutor(cap,
+				cap,
+				0L,
+				TimeUnit.MILLISECONDS,
+				new LinkedBlockingQueue<>(DEFAULT_BOUNDED_ELASTIC_QUEUESIZE),
+				threadFactory,
+				new ThreadPoolExecutor.AbortPolicy()
 		);
+		return Schedulers.fromExecutorService(readPool, name);
 	}
 
 	public Mono<Void> disposeGracefully() {
