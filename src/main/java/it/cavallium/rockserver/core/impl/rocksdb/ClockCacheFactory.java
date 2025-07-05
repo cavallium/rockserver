da078df0714bd75618e0bd8245eb6e943f4f6c90
Andrea Cavalli <andrea@cavallium.it>
2025-07-05 12:30:53 +0200

    Code cleanup, add HyperClockCache

--- src/main/java/it/cavallium/rockserver/core/impl/rocksdb/ClockCacheFactory.java
+++ src/main/java/it/cavallium/rockserver/core/impl/rocksdb/ClockCacheFactory.java
@@ -2,12 +2,13 @@ package it.cavallium.rockserver.core.impl.rocksdb;
 
 import org.rocksdb.Cache;
 import org.rocksdb.ClockCache;
+import org.rocksdb.HyperClockCache;
 
 public class ClockCacheFactory implements CacheFactory {
 
 	@Override
 	public Cache newCache(long size) {
-		return new ClockCache(size) {
+		return new HyperClockCache(size, 0, -1, false) {
 			{
 				RocksLeakDetector.register(this, "clock-cache", owningHandle_);
 			}
