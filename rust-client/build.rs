630379e963cc8f2e6c055c9fc0f66da60020ad2c
Andrea Cavalli <andrea@cavallium.it>
2025-12-27 22:36:17 +0100

    Add module, fix build.rs

--- rust-client/build.rs
+++ rust-client/build.rs
@@ -5,7 +5,28 @@ use rust_data_generator::schema::Schema;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Compile proto
-    tonic_build::compile_protos("proto/rocksdb.proto")?;
+    // Find protobuf include path from Maven artifacts
+    let proto_dep_dir = Path::new("../target/protoc-dependencies");
+    
+    let include_path = fs::read_dir(proto_dep_dir)
+        .ok()
+        .and_then(|entries| {
+            entries.flatten().find_map(|entry| {
+                let path = entry.path();
+                if path.is_dir() && path.join("google/protobuf/empty.proto").exists() {
+                    Some(path.to_string_lossy().into_owned())
+                } else {
+                    None
+                }
+            })
+        })
+        .ok_or("Could not find google/protobuf/empty.proto in ../target/protoc-dependencies. Ensure the Java project is built to generate dependencies.")?;
+
+    tonic_build::configure()
+        .compile_protos(
+            &["proto/rocksdb.proto"],
+            &["proto", &include_path],
+        )?;
 
     // Generate Datagen types using rust-data-generator
     let yaml_path = "examples/schema/database-records.yaml";
